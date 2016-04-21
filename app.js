c9ee4e4b72a3f351c9d1b8d27bcbb22eef68c73e
Liam DeCoste <liam@mediahack.com>
2016-04-21 18:58:10 -0500

    added node modules, started gm api

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var groupme = require('./routes/groupme');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/groupme', groupme);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/groupme.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+router.post('/', function(req, res) {
+    var body = req.body;
+
+    if(body['text'].toLowerCase().indexOf('alejandro') < 2){
+        res.send('{"containsName":"true"}');
+    }else{
+        res.send('{"containsName":"false"}');
+    }
+});
+
+module.exports = router;
