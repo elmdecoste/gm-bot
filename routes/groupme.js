f536e3e3d94ee557338d7b1ab8a88e35b3aaa393
Liam DeCoste <liam@mediahack.com>
2016-04-21 19:05:49 -0500

    added basic alejandro reply

--- routes/groupme.js
+++ routes/groupme.js
@@ -2,12 +2,18 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
+var botID= "3e535eaec161c3cf9d48d31fc3";
+
 router.post('/', function(req, res) {
     var body = req.body;
 
     console.log(body);
 
     if(body['text'].toLowerCase().indexOf('alejandro') < 2){
+
+        request.post({url:'https://api.groupme.com/v3/bots/post', form: {bot_id:botID, text:"You called?"}}, function(err,httpResponse,body){
+            console.log(httpResponse);
+        })
         res.send('{"containsName":"true"}');
     }else{
         res.send('{"containsName":"false"}');
