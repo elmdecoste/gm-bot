8d05750bc3ef454a065746318dbf9ff85cf0dc05
Liam DeCoste <liam@mediahack.com>
2016-04-21 19:00:26 -0500

    logs body of request

--- routes/groupme.js
+++ routes/groupme.js
@@ -5,6 +5,8 @@ var request = require('request');
 router.post('/', function(req, res) {
     var body = req.body;
 
+    console.log(body);
+
     if(body['text'].toLowerCase().indexOf('alejandro') < 2){
         res.send('{"containsName":"true"}');
     }else{
